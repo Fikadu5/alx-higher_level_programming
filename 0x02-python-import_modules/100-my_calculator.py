2feb0050947e01a51b795bb4361de322b1b30dd2
Fikadu5 <fikadugetnet428@gmail.com>
2023-10-05 11:50:06 -0700

    Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+
     args = sys.argv
     if len(args) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
